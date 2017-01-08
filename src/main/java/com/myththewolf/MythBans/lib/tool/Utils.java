987d23522dc6289f873fb95ef637e86d6350510d
MythTheWolf <myth.wolf6@gmail.com>
2017-01-08 15:46:59 -0600

    Updated default ban reason stuff

--- src/main/java/com/myththewolf/MythBans/lib/tool/Utils.java
+++ src/main/java/com/myththewolf/MythBans/lib/tool/Utils.java
@@ -1,5 +1,7 @@
 package com.myththewolf.MythBans.lib.tool;
 
+import com.myththewolf.MythBans.lib.feilds.ConfigProperties;
+
 public class Utils {
 	public static String makeString(String[] args, int index) {
 		String myString = ""; // we're going to store the arguments here
@@ -9,6 +11,10 @@ public class Utils {
 			String arg = args[i] + " "; // get the argument, and add a space so
 										// that the words get spaced out
 			myString = myString + arg; // add the argument to myString
+		}
+		if(myString == null || myString.equals(" ") || myString.equals(""))
+		{
+			myString = ConfigProperties.DEFAULT_BAN_REASON;
 		}
 		return myString;
 	}
