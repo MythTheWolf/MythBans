7fe46eb21b7f11597e8e47700c990e7f9cf5d5e0
MythTheWolf <myth.wolf6@gmail.com>
2017-02-05 17:15:07 -0600

    Plz work

--- src/main/java/com/myththewolf/MythBans/commands/createUI.java
+++ src/main/java/com/myththewolf/MythBans/commands/createUI.java
@@ -10,7 +10,6 @@ import net.md_5.bungee.api.ChatColor;
 
 public class createUI implements CommandExecutor {
 
-	@Override
 	public boolean onCommand(CommandSender sender, Command arg1, String arg2, String[] args) {
 		if (args.length < 0) {
 			sender.sendMessage(ConfigProperties.PREFIX + ChatColor.RED + "Usage: /createUI <player>");
