5e1c12ea39c8cefb5dfc14d1babedad52d2912c9
MythTheWolf <myth.wolf6@gmail.com>
2017-02-22 21:23:37 -0600

    things

--- src/main/java/com/myththewolf/MythBans/lib/player/PlayerTicket.java
+++ src/main/java/com/myththewolf/MythBans/lib/player/PlayerTicket.java
@@ -3,12 +3,21 @@ package com.myththewolf.MythBans.lib.player;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class PlayerTicket {
 	private PreparedStatement ps;
 	private Connection con;
 	private ResultSet rs;
-	public void recordGreif(String UUID, String timestamp, String location, String text){
-		
+
+	public void recordGreif(String UUID, String timestamp, String location, String text) throws SQLException {
+		ps = (PreparedStatement) con.prepareStatement(
+				"INSERT INTO MythBans_Tickets (`PRIORITY`,`SENDER_UUID`,`status`,`message`,`location`) VALUES (?,?,?,?,?)");
+		ps.setString(1, "HIGH");
+		ps.setString(2, UUID);
+		ps.setString(3, "OPEN");
+		ps.setString(4, text);
+		ps.setString(5, location);
+		ps.executeUpdate();
 	}
 }
